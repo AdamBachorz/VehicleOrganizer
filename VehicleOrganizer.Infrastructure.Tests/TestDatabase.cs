5c04bdbc4c8e06489caac17b33c41a985d704b15
AdamBachorz <adam.bachorz1702@gmail.com>
2023-10-04 22:59:43 +0200

    Add TestDatabase

---
+++ VehicleOrganizer.Infrastructure.Tests/TestDatabase.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VehicleOrganizer.Infrastructure.Tests;
+
+public static class TestDatabase
+{
+    public static void Reset()
+    {
+        CreateContext().Database.EnsureDeleted();
+    }
+
+    public static DataBaseContext CreateContext(bool allowLazyLoading = false)
+    {
+        var services = new ServiceCollection();
+        services.AddTestDatabaseContext();
+        services.AddLogging();
+        var sp = services.BuildServiceProvider();
+        var context = sp.GetRequiredService<DataBaseContext>();
+
+        context.ChangeTracker.LazyLoadingEnabled = allowLazyLoading;
+
+        return context;
+    }
+
+    public static DataBaseContext GetTestContext(this IFixture fixture) => CreateContext(true);
+
+    public static void AddTestDatabaseContext(this IFixture fixture, bool allowLazyLoading = false)
+    {
+        Reset();
+        fixture.Register(() => CreateContext(allowLazyLoading));
+    }
+
+    public static IServiceCollection AddTestDatabaseContext(this IServiceCollection services)
+    {
+        services.AddDbContext<DataBaseContext>(builder =>
+        {
+            builder.UseInMemoryDatabase("Dummy");
+            //builder.UseLazyLoadingProxies();
+        });
+
+        return services;
+    }
+}
