57c8c4dd1dd2db367e3c8aeae3a74378aca4a3b5
AdamBachorz <adam.bachorz1702@gmail.com>
2023-10-21 23:30:33 +0200

    Add EmailService facade

---
+++ VehicleOrganizer.Infrastructure.Tests/Services/Email/EmailSenderServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using VehicleOrganizer.Infrastructure.Services.Email;
+
+namespace VehicleOrganizer.Infrastructure.Tests.Services.Email
+{
+    public class EmailSenderServiceTests : BaseTests
+    {
+        private EmailSenderService _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            base.Setup();
+
+            _sut = new EmailSenderService();
+        }
+
+        [Test]
+        [Explicit]
+        public void ShouldSendEmail_SendEmail()
+        {
+            _sut.SendEmail("Testowy temat", "testowa treść <h1>Nagłówek</h1>");
+
+            Assert.Pass();
+        }
+    }
+}
---
+++ VehicleOrganizer.Infrastructure/Services/Email/EmailSenderService.cs
@@ -0,0 +1,39 @@
+﻿using FluentNHibernate.Conventions.Instances;
+using mailslurp.Api;
+using mailslurp.Client;
+using mailslurp.Model;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using VehicleOrganizer.Infrastructure.Entities;
+
+namespace VehicleOrganizer.Infrastructure.Services.Email
+{
+    public class EmailSenderService
+    {
+        private InboxControllerApi _controller;
+        private InboxDto _inbox;
+
+        public EmailSenderService()
+        {
+            //Fail
+            // first configure your api key
+            var config = new Configuration();
+            config.ApiKey.Add("x-api-key", "6a17b5e7251cd259aafd58675991c0d1b1dbcec5162b428e5ada8e6c7e2ea9b1");
+
+            _controller = new InboxControllerApi(config);
+            _inbox = _controller.CreateInbox();         
+        }
+
+        public void SendEmail(string subject, string body)
+        {
+            var sendEmailOptions = new SendEmailOptions()
+            {
+                To = new List<string>() { User.Default.Email },
+                Subject = subject,
+                Body = body
+            };
+            _controller.SendEmail(_inbox.Id, sendEmailOptions);
+        }
+    }
+}
